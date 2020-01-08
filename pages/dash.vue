7b893c6805a04b2de05850ef4a271506e04f4199
Waclaw Bielecki <hardhill@mail.ru>
2020-01-08 21:21:02 +0800

    главная страница

--- pages/dash.vue
+++ pages/dash.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <AppBar/>
-    <h1>Super secret page</h1>
-    <p>If you try to access this URL not connected, you will see the error
-       page telling your that you are not connected.</p>
+    
     <NuxtLink to="/">
-      Back to the home page
+      Вернуться на главную страницу
     </NuxtLink>
   </div>
 </template>
